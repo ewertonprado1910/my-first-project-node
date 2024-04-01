3a0617f87401c59a664fca0ccea3e734979908b1
ewertonprado1910 <ewertonprado20@gmail.com>
2024-04-01 11:22:35 -0300

    First project node

--- index.js
+++ index.js
@@ -1,70 +1,61 @@
-const express = require("express")
-const uuid = require("uuid")
+import express from "express";
+import { v4 as uuidv4 } from "uuid"; 
+import cors from "cors";
 
+const port = 3001;
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const port = 3000
-const app = express()
-app.use(express.json())
-
-const users = []
+const users = [];
 
 const checkUserId = (request, response, next) => {
-   const { id } = request.params
-   const index = users.findIndex(user => user.id === id)
+  const { id } = request.params;
+  const index = users.findIndex((user) => user.id === id);
 
-   if (index < 0) {
-      return response.status(404).json({ error: "Usuario não econtrado" })
-   }
+  if (index < 0) {
+    return response.status(404).json({ error: "Usuário não encontrado" });
+  }
 
-   request.UserIndex = index
-   request.userId = id
+  request.UserIndex = index;
+  request.userId = id;
 
-   next()
-
-}
+  next();
+};
 
 app.get("/users", (request, response) => {
-   return response.json(users)
-})
+  return response.json(users);
+});
 
 app.post("/users", (request, response) => {
-   const { name, age } = request.body
+  const { name, age } = request.body;
 
-   const user = { id: uuid.v4(), name, age }
+  const user = { id: uuidv4(), name, age }; 
 
-
-   users.push(user)
-   return response.status(201).json(user)
-
-})
+  users.push(user);
+  return response.status(201).json(user);
+});
 
 app.put("/users/:id", checkUserId, (request, response) => {
-   const { name, age } = request.body
-   const index = request.UserIndex
-   const id = request.userId
-   
-   const updateUser = { id, name, age }
+  const { name, age } = request.body;
+  const index = request.UserIndex;
+  const id = request.userId;
 
-   users[index] = updateUser
+  const updateUser = { id, name, age };
 
-   return response.status(201).json(updateUser)
+  users[index] = updateUser;
 
-})
+  return response.status(201).json(updateUser);
+});
 
-app.delete("/users/:id", checkUserId,(request, response) => {
-   const index = request.UserIndex
+app.delete("/users/:id", checkUserId, (request, response) => {
+  const index = request.UserIndex;
 
-   users.splice(index, 1)
+  users.splice(index, 1);
 
-   return response.status(204).json()
-})
-
-
-
-
-
-
+  return response.status(204).json();
+});
 
 app.listen(port, () => {
-   console.log(`Servidor online, port ${port}`)
-})
+  console.log(`Online server port ${port}`);
+});
